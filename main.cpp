bcfb04ea3dabe6ceddaa4679af0fc9da62784f07
Tony Xiang <tony.xiang@alliander.com>
2024-03-29 10:41:39 +0100

    factorize
    Signed-off-by: Tony Xiang <tony.xiang@alliander.com>

--- main.cpp
+++ main.cpp
@@ -1,5 +1,30 @@
 #include <iostream>
+#include <Eigen/Dense>
+
+
+void factorize(Eigen::Matrix3d const& matrix) {
+    std::cout << "============\nOriginal matrix\n";
+    std::cout << matrix << '\n';
+
+    Eigen::PartialPivLU<Eigen::Matrix3d> partial_lu{matrix};
+    std::cout << "Partial pivot\n";
+    std::cout << partial_lu.matrixLU() << '\n';
+
+    Eigen::FullPivLU<Eigen::Matrix3d> full_lu{matrix};
+    std::cout << "Full pivot\n";
+    std::cout << full_lu.matrixLU() << '\n';
+
+    std::cout << "\n\n";
+}
+
 
 int main() {
-    std::cout << "Hi!\n";
+    Eigen::Matrix3d matrix;
+
+    matrix << 
+        1, 2, 3,
+        4, 5, 6,
+        7, 8, 9;
+    
+    factorize(matrix);
 }
